de66758d6d5064f57ecfab4cc67f678873a973d4
Ilaria Callegari <ilaria.callegari@gmail.com>
2020-06-26 16:45:18 +0100

    finish navigation, refactor about us, image composition

--- assets/script.js
+++
@@ -1,6 +0,0 @@
-const menu = document.querySelector('#hamburger-menu');
-const toggleNav = document.querySelector('.toggle-nav');
-
-menu.addEventListener('click', () => {
-    toggleNav.classList.toggle('responsive');
-});
